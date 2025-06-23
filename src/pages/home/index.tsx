090f1c38e90dfc0f2362348317b0c9b4a79397b1
ynurr <0305_08@naver.com>
2025-06-24 05:09:02 +0900

    feat: 페이지 이동 및 ID 표시 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { useListQuery } from "../../queries/useListQuery";
 import { TYPE_COLORS } from "../../constants/color";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
 
@@ -9,23 +10,26 @@ const HomePage = () => {
     return (
         <Wrapper>
             <Title>포켓몬 도감</Title>
-            <SearchInput placeholder="포켓몬 이름 또는 번호를 이력해주세요" />
+            <SearchInput placeholder="포켓몬 이름 또는 번호를 입력해주세요" />
             <CardGrid>
                 {data?.map((pokemon, i:number) => (
-                    <Card key={i}>
-                        <ImageBox>
-                            <img
-                                src={pokemon.image}
-                                alt={pokemon.name}
-                            />
-                        </ImageBox>
-                        <Name>{pokemon.name}</Name>
-                        {pokemon.types.map((t) => (
-                            <Type key={t.type_en} $type={t.type_en}>
-                            {t.type_ko}
-                            </Type>
-                        ))}
-                    </Card>
+                    <StyledLink to={`/detail/${pokemon.id}`} key={pokemon.id}>
+                        <Card>
+                            <ImageBox>
+                                <img
+                                    src={pokemon.image}
+                                    alt={pokemon.name}
+                                />
+                            </ImageBox>
+                            <Id>#{pokemon.id}</Id>
+                            <Name>{pokemon.name}</Name>
+                            {pokemon.types.map((t) => (
+                                <Type key={t.type_en} $type={t.type_en}>
+                                {t.type_ko}
+                                </Type>
+                            ))}
+                        </Card>
+                    </StyledLink>
                 ))}
             </CardGrid>
         </Wrapper>
@@ -33,16 +37,6 @@ const HomePage = () => {
 }
 
 export default HomePage;
-
-const Type = styled.span<{ $type: string }>`
-    margin: 10px 3px 0;
-    display: inline-block;
-    padding: 2px 8px;
-    border-radius: 8px;
-    background-color: ${({ $type }) => TYPE_COLORS[$type.toLowerCase()]};
-    color: #ffffff;
-    font-weight: 500;
-`
 
 const Wrapper = styled.div`
     padding: 100px 200px;
@@ -54,44 +48,76 @@ const Title = styled.h1`
 `
 
 const SearchInput = styled.input`
-    padding: 10px 14px;
+    padding: 10px 18px;
     width: 100%;
-    max-width: 400px;
+    max-width: 340px;
     margin-bottom: 2rem;
-    border: 1px solid #ccc;
-    border-radius: 8px;
+    // border: 1px solid #ccc;
+    border: none;
+    border-radius: 20px;
+    background-color:rgb(236, 238, 240);
+    &:focus {
+        outline: none;
+        placeholder: none;
+    }
+    &::placeholder {
+        color: #999;
+    }
 `
 
 const CardGrid = styled.div`
     display: grid;
     justify-content: center;
-    grid-template-columns: repeat(auto-fit, 180px);
-    gap: 20px;
+    grid-template-columns: repeat(auto-fit, 170px);
+    gap: 30px;
 `
 
 const Card = styled.div`
-    width: 180px;
+    width: 170px;
     padding: 1rem;
     background-color: #ffffff;
-    border-radius: 8px;
+    border-radius: 30px;
     text-align: center;
     cursor: pointer;
     transition: transform 0.2s ease, box-shadow 0.2s ease;
-
     &:hover {
         transform: scale(1.05);
         box-shadow: 0 4px 12px rgba(0, 0, 0, 0.08);
     }
 `
-    
+
 const ImageBox = styled.div`
     width: 100%;
     margin-bottom: 0.5rem;
     display: flex;
     justify-content: center;
     align-items: center;
+    img {
+        width: 120px;
+        height: 120px;
+    }
 `
+
+const Id = styled.p``
 
 const Name = styled.p`
     font-weight: bold;
+`
+
+const Type = styled.span<{ $type: string }>`
+    margin: 10px 3px 0;
+    display: inline-block;
+    padding: 2px 8px;
+    border-radius: 8px;
+    background-color: ${({ $type }) => TYPE_COLORS[$type.toLowerCase()]};
+    color: #ffffff;
+    font-weight: 500;
+`
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: inherit;
+    &:hover {
+        color: inherit;
+    }
 `
